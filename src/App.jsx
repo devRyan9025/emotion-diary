a0b80a55c55ce1f7ee75ef288947a57820c47c9b
devRyan9025 <alex7006@naver.com>
2024-10-31 11:51:21 +0900

    feat : Add common components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import './App.css';
-import {
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import NotFound from './pages/NotFound';
-import { getEmotionImage } from './util/get-emotion-image';
+import Button from './components/Button';
+import Header from './components/Header';
 
 // <Routes로 경로 설정하기>
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
@@ -17,27 +13,34 @@ import { getEmotionImage } from './util/get-emotion-image';
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav('/new');
-  };
-
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/new'}>New</Link>
-        <Link to={'/diary'}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={'Header'}
+        leftChild={<Button text={'<'} />}
+        rightChild={<Button text={'>'} />}
+      />
+      <Button
+        text={'123'}
+        onClick={() => {
+          console.log('123번 버튼 클릭');
+        }}
+      />
+      <Button
+        text={'123'}
+        type={'POSITIVE'}
+        onClick={() => {
+          console.log('123번 버튼 클릭');
+        }}
+      />
+      <Button
+        text={'123'}
+        type={'NEGATIVE'}
+        onClick={() => {
+          console.log('123번 버튼 클릭');
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
