9bc4a4167f4876713f27803ef7f7a2c3d010beb2
devRyan9025 <alex7006@naver.com>
2024-11-01 09:09:34 +0900

    feat : Add the edit page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
+import Edit from './pages/Edit';
 import NotFound from './pages/NotFound';
 import Button from './components/Button';
 import Header from './components/Header';
@@ -11,40 +12,16 @@ import Header from './components/Header';
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" :  새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
+// 4. "/edit" : 일기를 수정하는 Edit 페이지
 
 function App() {
   return (
     <>
-      <Header
-        title={'Header'}
-        leftChild={<Button text={'<'} />}
-        rightChild={<Button text={'>'} />}
-      />
-      <Button
-        text={'123'}
-        onClick={() => {
-          console.log('123번 버튼 클릭');
-        }}
-      />
-      <Button
-        text={'123'}
-        type={'POSITIVE'}
-        onClick={() => {
-          console.log('123번 버튼 클릭');
-        }}
-      />
-      <Button
-        text={'123'}
-        type={'NEGATIVE'}
-        onClick={() => {
-          console.log('123번 버튼 클릭');
-        }}
-      />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
