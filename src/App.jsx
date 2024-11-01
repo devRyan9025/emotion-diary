1e66df4e5b440a430671cf06aa5610b73bf0fbfe
devRyan9025 <alex7006@naver.com>
2024-11-01 10:34:27 +0900

    feat: Add mockData,diary functions and context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,101 @@
 import './App.css';
+import { useReducer, useRef, createContext } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 import NotFound from './pages/NotFound';
-import Button from './components/Button';
-import Header from './components/Header';
 
-// <Routes로 경로 설정하기>
-// 1. "/" : 모든 일기를 조회하는 Home 페이지
-// 2. "/new" :  새로운 일기를 작성하는 New 페이지
-// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-// 4. "/edit" : 일기를 수정하는 Edit 페이지
+// 임시 데이터
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+];
+
+// state를 관리하는 reducer
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return [action.data, ...state];
+    case 'UPDATE':
+      return state.map((item) =>
+        String(item.id) === String(action.data.id)
+          ? action.data
+          : item
+      );
+    case 'DELETE':
+      return state.filter(
+        (item) => String(item.id) !== String(action.id)
+      );
+
+    default:
+      return state;
+  }
+}
+
+// 일기 데이터를 공급해주는 stateContext
+export const DiaryStateContext = createContext();
+
+// 추가,수정,삭제 등 기능을 공급해주는 dispatchContext
+export const DiaryDispatchContext = createContext();
 
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 새로운 일기를 추가
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      },
+    });
+  };
+
+  // 기존의 일기를 수정
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: 'UPDATE',
+      data: { id, createdDate, emotionId, content },
+    });
+  };
+
+  // 기존 일기를 삭제
+  const onDelete = (id) => {
+    dispatch({
+      type: 'DELETE',
+      id,
+    });
+  };
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/new" element={<New />} />
-        <Route path="/diary/:id" element={<Diary />} />
-        <Route path="/edit/:id" element={<Edit />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider
+          value={(onCreate, onUpdate, onDelete)}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/diary/:id" element={<Diary />} />
+            <Route path="/edit/:id" element={<Edit />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   );
 }
