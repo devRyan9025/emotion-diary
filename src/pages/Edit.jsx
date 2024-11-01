e9948217bfc40d6f7e79a711f2c76ee5fbe232f0
devRyan9025 <alex7006@naver.com>
2024-11-01 10:28:58 +0900

    refactor : Add useParams to identify each diary

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,5 +1,8 @@
+import { useParams } from 'react-router-dom';
+
 const Edit = () => {
-  return <div>edit</div>;
+  const params = useParams('');
+  return <div>{params.id}번 페이지입니다.</div>;
 };
 
 export default Edit;
